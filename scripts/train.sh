20294efed33d42265f1b7bc00777c592f7afce37
郑煜伟 <7124573+zheng-yuwei@users.noreply.github.com>
2021-08-21 13:27:18 +0800

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -8,7 +8,7 @@ CUDA_VISIBLE_DEVICES=0 python main.py --dataset YOUR_DATA_SET --result_dir resul
 
 # 背景不变
 CUDA_VISIBLE_DEVICES=0 python main.py --dataset YOUR_DATA_SET --result_dir results --img_size 256 \
---seg_weight 50 --seg_rand_mask 0.8
+--cam_D_weight -1 --cam_D_attention --seg_fix_weight 100 --seg_D_mask --seg_G_detach
 
 # se + blur
 CUDA_VISIBLE_DEVICES=0 python main.py --dataset YOUR_DATA_SET --result_dir results --img_size 256 --use_se --has_blur
@@ -19,7 +19,7 @@ CUDA_VISIBLE_DEVICES=0 python main.py --dataset YOUR_DATA_SET --result_dir resul
 
 # 损失权重调整
 CUDA_VISIBLE_DEVICES=0 python main.py --dataset YOUR_DATA_SET --result_dir results --img_size 256 \
---adv_weight 0.8 --forward_adv_weight 2 --cycle_weight 5 --identity_weight 5
+--adv_weight 1.0 --forward_adv_weight 2 --cycle_weight 5 --identity_weight 5
 
 # cpu调试
 python main.py --dataset YOUR_DATA_SET --result_dir results --img_size 256 --device cpu --num_workers 0
@@ -27,5 +27,6 @@ python main.py --dataset YOUR_DATA_SET --result_dir results --img_size 256 --dev
 # 复杂cpu测试：直方图匹配 + 背景不变 + se + blur + attention + 原图
 python main.py --dataset YOUR_DATA_SET --result_dir results --img_size 256 --device cpu --num_workers 0 \
 --match_histograms --match_mode hsl --match_prob 0.5 --match_ratio 1.0 \
---seg_weight 50 --seg_rand_mask 0.8 --use_se --has_blur --attention_gan 3 --attention_input \
---adv_weight 0.8 --forward_adv_weight 2 --cycle_weight 20 --identity_weight 5
+--cam_D_weight -1 --cam_D_attention --seg_fix_weight 100 --seg_D_mask --seg_G_detach \
+--use_se --has_blur --attention_gan 3 --attention_input \
+--adv_weight 1.0 --forward_adv_weight 2 --cycle_weight 20 --identity_weight 5
